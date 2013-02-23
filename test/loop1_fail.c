e35bf34dd2a59da8abffcb54897a9f3eed7e6db5
Zvonimir <zvonimir@cs.utah.edu>
2013-02-22 19:27:56 -0700

    Moved simple loop example to regressions.

---
+++ test/loop1_fail.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "smack.h"
+
+#define MAXSIZE 10
+
+int x;
+
+int main() {
+  int i = 0, j;
+
+  x = 1;
+
+  for (i = 0; i < MAXSIZE; i++) {
+    j = i;
+  }
+  __SMACK_assert(x != 1);
+  return 0;
+}
+
