10b16a56557c639a5b3bf22c0fc4d63ce883a3a4
zvonimir <zvonimir@cs.utah.edu>
2013-06-23 10:48:04 -0600

    Updated other regtest scripts to test for both memory models.

--- examples/svcomp13/locks/regtest.py
+++ examples/svcomp13/locks/regtest.py
@@ -20,21 +20,30 @@ tests = [
   ('test_locks_15_unsafe',  r'0 verified, 1 error' )
 ]
 
+def red(text):
+  return '\033[0;31m' + text + '\033[0m'
+  
+def green(text):
+  return '\033[0;32m' + text + '\033[0m'
 
 passed = failed = 0
 for test in tests:
+    
+  for mem in ['flat', 'twodim']:
+    
+    print "{0:>20} {1:>8}:".format(test[0], "(" + mem + ")"),
 
-  # invoke SMACK
-  p = subprocess.Popen(['smack-check.py', test[0] + '.o', '-o', test[0] +'.bpl'], stdout=subprocess.PIPE)
-  smackOutput = p.communicate()[0]
+    # invoke SMACK
+    p = subprocess.Popen(['smack-check.py', test[0] + '.o', '--mem-mod=' + mem, '-o', test[0] +'.bpl'], stdout=subprocess.PIPE)
+    smackOutput = p.communicate()[0]
 
-  # check SMACK output
-  if re.search(test[1], smackOutput):
-    print 'PASSED: ', test[0]
-    passed += 1
-  else:
-    print 'FAILED: ', test[0]
-    failed += 1
+    # check SMACK output
+    if re.search(test[1], smackOutput):
+      print green('PASSED')
+      passed += 1
+    else:
+      print red('FAILED')
+      failed += 1
 
 print '\nPASSED count: ', passed
 print 'FAILED count: ', failed
