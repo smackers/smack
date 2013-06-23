6579f447d30b97a56bb4dd035dc3ea01acf7146d
Michael Emmi <michael.emmi@gmail.com>
2013-06-23 13:49:58 +0200

    Regressions testing both Flat and 2D memories.

--- test/regtest.py
+++ test/regtest.py
@@ -63,21 +63,30 @@ tests = [
   ('two_arrays6_fail',   r'0 verified, 1 error' )
 ]
 
+def red(text):
+  return '\033[0;31m' + text + '\033[0m'
+  
+def green(text):
+  return '\033[0;32m' + text + '\033[0m'
 
 passed = failed = 0
 for test in tests:
+    
+  for mem in ['flat', 'twodim']:
+    
+    print "{0:>20} {1:>8}:".format(test[0], "(" + mem + ")"),
 
-  # invoke SMACK
-  p = subprocess.Popen(['smack-check.py', test[0] + '.o', '-o', test[0] +'.bpl'], stdout=subprocess.PIPE)
-  smackOutput = p.communicate()[0]
+    # invoke SMACK
+    p = subprocess.Popen(['smack-check.py', test[0] + '.o', '--mem-mod=' + mem, '-o', test[0] +'.bpl'], stdout=subprocess.PIPE)
+    smackOutput = p.communicate()[0]
 
-  # check SMACK output
-  if re.search(test[1], smackOutput):
-    print 'PASSED: ', test[0]
-    passed += 1
-  else:
-    print 'FAILED: ', test[0]
-    failed += 1
+    # check SMACK output
+    if re.search(test[1], smackOutput):
+      print green('PASSED')
+      passed += 1
+    else:
+      print red('FAILED')
+      failed += 1
 
 print '\nPASSED count: ', passed
 print 'FAILED count: ', failed
