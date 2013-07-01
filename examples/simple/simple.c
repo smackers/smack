15e3727f2630681f7b61850beba73ff389021dd9
Zvonimir <zvonimir@cs.utah.edu>
2013-07-01 10:31:28 -0600

    Shuffled examples around.

---
+++ examples/simple/simple.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "smack.h"
+
+int incr(int x) {
+  return x + 1;
+}
+
+int main(void) {
+  int a;
+
+  a = 1;
+  a = incr(a);
+  __SMACK_assert(a == 2);
+  return 0;
+}
+
