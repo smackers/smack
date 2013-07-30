4c57e7416f1ed8c62f82df4ebe9e49c97cacae74
Zvonimir <zvonimir@cs.utah.edu>
2013-07-30 13:22:38 -0600

    Reorganized SMACK scripts.

--- bin/smack.py
+++ bin/smack.py
@@ -10,6 +10,15 @@ from llvm2bpl import *
 VERSION = '1.2'
 
 
+def smackParser():
+  parser = argparse.ArgumentParser(add_help=False, parents=[llvm2bplParser()])
+  parser.add_argument('--checker', dest='checker', choices=['boogie-plain', 'boogie-inline', 'corral'], default='boogie-inline',
+                      help='set the underlying checker format')
+  parser.add_argument('--entry-points', metavar='PROC', dest='entryPoints', default='main', nargs='+',
+                      help='specify entry procedures')
+  return parser
+
+
 def addInline(match, entryPoints):
   procName = match.group(1)
   procDef = ''
@@ -30,46 +39,51 @@ def addEntryPoint(match, entryPoints):
   return procDef
 
 
-if __name__ == '__main__':
-
-  # parse command line arguments
-  parser = argparse.ArgumentParser(description='Outputs the appropriately annotated Boogie file generated from the input LLVM file.', parents=[llvm2bplParser()])
-  parser.add_argument('--format', dest='format', choices=['boogie-plain', 'boogie-inline', 'corral'], default='boogie-plain',
-                      help='choose the appropriate output format')
-  parser.add_argument('--entry-points', metavar='PROC', dest='entryPoints', default='main', nargs='+',
-                      help='specify entry procedures')
-  args = parser.parse_args()
-
-  scriptPathName = path.dirname(sys.argv[0])
+def clang(scriptPathName, inputFile):
   scriptFullPath = path.abspath(scriptPathName)
   smackRoot = path.dirname(scriptFullPath)
   smackHeaders = path.join(smackRoot, 'include')
 
-  inputFile = args.infile
-  fileName, fileExtension = path.splitext(inputFile.name)
+  fileName = path.splitext(inputFile.name)[0]
 
+  p = subprocess.Popen(['clang', '-c', '-Wall', '-emit-llvm', '-O0', '-g',
+    '-I' + smackHeaders, inputFile.name, '-o', fileName + '.bc'])
+  p.wait()
+  inputFileName = path.join(path.curdir, fileName + '.bc')
+  inputFile = open(inputFileName, 'r')
+  return inputFile
+
+
+def smack(scriptPathName, inputFile, debugFlag, memmod, checker, entryPoints):
+  fileExtension = path.splitext(inputFile.name)[1]
   if fileExtension == '.c':
     # if input file is .c, then compile it first with clang
-    p = subprocess.Popen(['clang', '-c', '-Wall', '-emit-llvm', '-O0', '-g',
-      '-I' + smackHeaders, inputFile.name, '-o', fileName + '.bc'])
-    p.wait()
-    inputFileName = path.join(path.curdir, fileName + '.bc')
-    inputFile = open(inputFileName, 'r')
+    inputFile = clang(scriptPathName, inputFile)
 
-  debug, bpl = llvm2bpl(scriptPathName, inputFile, args.debug, args.memmod)
+  debug, bpl = llvm2bpl(scriptPathName, inputFile, debugFlag, memmod)
   inputFile.close()
 
   # print debug info
-  if args.debug:
+  if debugFlag:
     print debug
 
   p = re.compile('procedure[ ]*([a-zA-Z0-9_]*)[ ]*\(')
-  if args.format == 'boogie-inline':
+  if checker == 'boogie-inline':
     # put inline on procedures
-    bpl = p.sub(lambda match: addInline(match, args.entryPoints), bpl)
-  elif args.format == 'corral':
+    bpl = p.sub(lambda match: addInline(match, entryPoints), bpl)
+  elif checker == 'corral':
     # annotate entry points
-    bpl = p.sub(lambda match: addEntryPoint(match, args.entryPoints), bpl)
+    bpl = p.sub(lambda match: addEntryPoint(match, entryPoints), bpl)
+  return bpl
+
+
+if __name__ == '__main__':
+
+  # parse command line arguments
+  parser = argparse.ArgumentParser(description='Outputs the appropriately annotated Boogie file generated from the input LLVM file.', parents=[smackParser()])
+  args = parser.parse_args()
+
+  bpl = smack(path.dirname(sys.argv[0]), args.infile, args.debug, args.memmod, args.checker, args.entryPoints)
 
   # write final output
   args.outfile.write(bpl)
