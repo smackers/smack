491bda21a7aecd863504753fad7bc7b7238d388a
Zvonimir <zvonimir@cs.utah.edu>
2013-10-09 09:42:00 -0600

    Added SVCOMP-specific include file that defines VERIFIER functions. Fixes #23.

--- examples/svcomp13/ntdrivers-simplified/floppy_simpl3_safe.cil.c
+++ examples/svcomp13/ntdrivers-simplified/floppy_simpl3_safe.cil.c
@@ -1,4 +1,4 @@
-#include "smack.h"
+#include "smack-svcomp.h"
 extern char __VERIFIER_nondet_char(void);
 extern int __VERIFIER_nondet_int(void);
 extern long __VERIFIER_nondet_long(void);
---
+++ examples/svcomp13/ntdrivers-simplified/smack-svcomp.h
@@ -0,0 +1,22 @@
+#ifndef SMACKSVCOMP_H_
+#define SMACKSVCOMP_H_
+#include "smack.h"
+
+char __VERIFIER_nondet_char(void) {
+  return __SMACK_nondet();
+}
+
+int __VERIFIER_nondet_int(void) {
+  return __SMACK_nondet();
+}
+
+long __VERIFIER_nondet_long(void) {
+  return __SMACK_nondet();
+}
+
+void *__VERIFIER_nondet_pointer(void) {
+  return __SMACK_nondet();
+}
+
+#endif /*SMACKSVCOMP_H_*/
+
