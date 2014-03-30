f26a5a4bf0301fb60c67ed23c45c3a17ab713989
Michael Emmi <michael.emmi@gmail.com>
2014-03-30 17:43:35 +0200

    Added failing example with cmpxchg instruction.

---
+++ examples/failing/atomic_cas.c
@@ -0,0 +1,12 @@
+#include <smack.h>
+
+#define CAS(x,y,z) __atomic_compare_exchange_n(x,&(y),z,true,0,0)
+
+int main() {
+	int *x = 0;
+  int y = 0;
+  int *z = x;
+  CAS(&z,x,&y); // if (z == x) z = &y;
+  __SMACK_assert(*z == y);
+  return 0;
+}
