e08e5fafffdc5db7e962f1237b2bc69b78ed79b4
Arvind Haran <arvind1990@github.com>
2014-10-09 14:34:52 -0600

    Overloaded the assume and nondet function for a few types

--- include/smack/smack-svcomp.h
+++ include/smack/smack-svcomp.h
@@ -8,4 +8,20 @@ void __VERIFIER_error(void) {
   assert(0);
 }
 
+void __VERIFIER_assume(int v) {
+  assume(v);
+}
+
+//Types to be overloaded for: {bool, char, int, float, loff_t, long, pchar, pointer, pthread_t, sector_t, short, size_t, u32, uchar, uint, ulong, unsigned, ushort}
+
+char __VERIFIER_nondet_char() {
+	return (char)__SMACK_nondet();
+}
+
+int __VERIFIER_nondet_int() {
+	return __SMACK_nondet();
+}
+
+
+
 #endif
