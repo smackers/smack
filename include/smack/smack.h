e4ccb7eaac5b3406f21e88309838d0e2f5bbe54b
Zvonimir <zvonimir@cs.utah.edu>
2015-01-23 11:37:47 -0700

    Cleaned up smack.h - removed some commented out code we are not using any more. Closes #63.

--- include/smack/smack.h
+++ include/smack/smack.h
@@ -47,13 +47,6 @@ void __SMACK_dummy(int v) {
 #define assert(EX) __SMACK_dummy(EX); __SMACK_code("assert @ != 0;", EX)
 #define assume(EX) __SMACK_dummy(EX); __SMACK_code("assume @ != 0;", EX)
 
-//// PROBLEM: in the 2D memory model, the declaration of boogie_si_record_int
-//// should have a type $ptr parameter, not an int.  How should we do this?
-// void __SMACK_record_int(int i) {
-//   __SMACK_top_decl("procedure boogie_si_record_int(i:int);");
-//   __SMACK_code("call boogie_si_record_int(@);", i);
-// }
-
 int __SMACK_nondet() {
   static int XXX;
   int x = XXX;
