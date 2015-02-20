6fc254de2d1251d2bc54e09b02d3441f1dbc223f
Zvonimir <zvonimir@cs.utah.edu>
2015-02-20 11:20:31 -0700

    Split smack-svcomp into a header file and an implementation file.

--- share/smack/include/smack-svcomp.h
+++ share/smack/include/smack-svcomp.h
@@ -12,64 +12,23 @@
 #define __builtin_va_start __builtinx_va_start // Rewrite so that clang does not complain
 #define __builtin_object_size __builtinx_object_size // Rewrite so that clang does not complain
 
-void __VERIFIER_error(void) {
-  assert(0);
-}
+void __VERIFIER_error(void);
+void __VERIFIER_assume(int);
+void exit(int);
 
-void __VERIFIER_assume(int v) {
-  assume(v);
-}
-
-void exit(int x) {
-  assume(0);
-}
 
 // Types to be overloaded for: {bool, float, loff_t, pchar,
 // pthread_t, sector_t, size_t, u32}
 
-char __VERIFIER_nondet_char(void) {
-  return (char)__SMACK_nondet();
-}
-
-short __VERIFIER_nondet_short(void) {
-  return (short)__SMACK_nondet();
-}
-
-int __VERIFIER_nondet_int(void) {
-  return __SMACK_nondet();
-}
-
-long __VERIFIER_nondet_long(void) {
-  return (long)__SMACK_nondet();
-}
-
-unsigned char __VERIFIER_nondet_uchar(void) {
-  char x = (char)__SMACK_nondet();
-  assume(x >= 0);
-  return (unsigned char)x;
-}
-
-unsigned short __VERIFIER_nondet_ushort(void) {
-  short x = (short)__SMACK_nondet();
-  assume(x >= 0);
-  return (unsigned short)x;
-}
-
-unsigned __VERIFIER_nondet_uint(void) {
-  int x = __SMACK_nondet();
-  assume(x >= 0);
-  return (unsigned)x;
-}
-
-unsigned long __VERIFIER_nondet_ulong(void) {
-  long x = (long)__SMACK_nondet();
-  assume(x >= 0);
-  return (unsigned long)x;
-}
-
-void* __VERIFIER_nondet_pointer(void) {
-  return (void*)__SMACK_nondet();
-}
+char __VERIFIER_nondet_char(void);
+short __VERIFIER_nondet_short(void);
+int __VERIFIER_nondet_int(void);
+long __VERIFIER_nondet_long(void);
+unsigned char __VERIFIER_nondet_uchar(void);
+unsigned short __VERIFIER_nondet_ushort(void);
+unsigned __VERIFIER_nondet_uint(void);
+unsigned long __VERIFIER_nondet_ulong(void);
+void* __VERIFIER_nondet_pointer(void);
 
 #endif
 
