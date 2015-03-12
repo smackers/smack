70735838bffe42400cece828b8cb2a98fed2c41b
Michael Emmi <michael.emmi@gmail.com>
2015-03-12 16:40:40 +0100

    Abridged list of Ubuntu 14.04 packages.

--- bin/build.sh
+++ bin/build.sh
@@ -206,14 +206,12 @@ if [ ${INSTALL_DEPENDENCIES} -eq 1 ]; then
     sudo add-apt-repository "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.5 main"
     wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
     sudo apt-get update
-    sudo apt-get install -y clang-3.5 clang-3.5-doc libclang-common-3.5-dev libclang-3.5-dev \
-      libclang1-3.5 libclang1-3.5-dbg libllvm3.5 libllvm3.5-dbg lldb-3.5 llvm-3.5 llvm-3.5-dev \
-      llvm-3.5-doc llvm-3.5-runtime lldb-3.5-dev
+    sudo apt-get install -y clang-3.5 libllvm3.5 llvm-3.5 llvm-3.5-dev llvm-3.5-runtime
     sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.5 20
     sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.5 20
     sudo update-alternatives --install /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-3.5 20
     sudo update-alternatives --install /usr/bin/llvm-link llvm-link /usr/bin/llvm-link-3.5 20
-    sudo apt-get install -y libz-dev libedit-dev mono-complete git mercurial cmake python-yaml
+    sudo apt-get install -y libz-dev libedit-dev mono-complete git mercurial cmake python-yaml unzip
     ;;
 
   linux-ubuntu-12*)
