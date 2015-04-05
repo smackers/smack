108b3f87597fd32a058d056814b1a19d34f1883d
Jonathan Whitaker <jon.b.whitaker@gmail.com>
2015-04-04 22:19:44 -0600

    Changed ProcessPool implementation to ThreadPool implementation. All logging should now be safe because the pool is implemented with threads.

--- test/regtest.py
+++ test/regtest.py
@@ -1,5 +1,11 @@
 #! /usr/bin/env python
 
+from os import path
+from multiprocessing.pool import ThreadPool
+import multiprocessing
+import os
+import signal
+import logging
 import yaml
 import argparse
 from os import path
@@ -11,6 +17,12 @@ import sys
 
 OVERRIDE_FIELDS = ['verifiers', 'memory', 'time-limit', 'skip']
 APPEND_FIELDS = ['flags']
+
+# integer constants
+PASSED = 0
+TIMEDOUT = 1
+UNKNOWN = 2
+FAILED = -1
 
 def red(text):
   return '\033[0;31m' + text + '\033[0m'
@@ -91,7 +103,9 @@ try:
       continue
 
     if meta['skip'] != False and not args.exhaustive:
-      continue
+      return
+
+    str_result = "{0:>20}\n".format(test)
 
     print "{0:>20}".format(test)
     sys.stdout.flush()
@@ -147,8 +161,115 @@ print
 print ' PASSED count:', passed
 print ' FAILED count:', failed
 
-if timeouts > 0:
-  print 'TIMEOUT count:', timeouts
+        # returns a status code based on the results of the test
+        if passed:
+            return PASSED
+        elif timedout:
+            return TIMEDOUT
+        elif unknown:
+            return UNKNOWN
+        else:
+            return FAILED
 
-if unknowns > 0:
-  print 'UNKNOWN count:', unknowns
+
+def init_worker():
+    """
+    Set up the worker processes to ignore SIGINT altogether. This confines all
+    the cleanup code to the parent process.
+
+    Reference: http://stackoverflow.com/a/6191991/3342427
+    """
+    #signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+def main():
+    """
+    Main entry point for the test suite.
+    """
+    
+    num_cpus = multiprocessing.cpu_count()
+
+    # configure the CLI
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--exhaustive", help="be exhaustive", action="store_true")
+    parser.add_argument("--threads", action="store", dest="n_threads", default=num_cpus, type=int,
+                        help="execute regressions using the selected number of threads in parallel")
+    parser.add_argument("--log", action="store", dest="log_level", default="DEBUG", type=str,
+                        help="sets the logging level (DEBUG, INFO, WARNING)")
+    parser.add_argument("--output_log", action="store", dest="log_path", type=str,
+                        help="sets the output log path. (std out by default)")
+    args = parser.parse_args()
+
+
+    # configure the logging
+    log_format = ''
+    log_level = logging.DEBUG
+
+    # add more log levels later (if needed)
+    if args.log_level.upper() == "INFO":
+        log_level = logging.INFO
+    elif args.log_level.upper() == "WARNING":
+        log_level = logging.WARNING
+
+    # if the user supplied a log path, write the logs to that file.
+    # otherwise, write the logs to std out.
+    if args.log_path:
+        logging.basicConfig(filename=args.log_path, format=log_format, level=log_level)
+    else:
+        logging.basicConfig(format=log_format, level=log_level)
+
+    logging.debug("Creating Pool with '%d' Workers" % args.n_threads)
+    p = ThreadPool(processes=args.n_threads, initializer=init_worker)
+
+    # start the tests
+    logging.info("Running regression tests...")
+    passed = failed = timeouts = unknowns = 0
+
+    # start processing the tests. this blocks the main thread
+    async_results = []
+    for test in glob.glob("./**/*.c"):
+        async_results.append(p.apply_async(process_test, args=(test, args,)))
+
+    try:
+        # keep the main thread active while there are active workers
+        count = 0
+        while True:
+            if count == len(async_results):
+                break;
+
+            # iterate over the results from each worker
+            for r in async_results:
+                try:
+                    # if the worker has completed its task, get the result
+                    # from it.
+                    if r.successful:
+                        test_result = r.get()
+                        if test_result == PASSED:
+                            passed += 1
+                        elif test_result == TIMEDOUT:
+                            timeouts += 1
+                        elif test_result == UNKNOWN:
+                            unknowns += 1
+                        elif test_result == FAILED:
+                            failed += 1
+                        count += 1
+                except AssertionError:
+                    continue
+
+    except KeyboardInterrupt:
+      logging.debug("Caught KeyboardInterrupt, terminating workers")
+      p.terminate() # terminate any remaining workers
+      p.join()
+    else:
+      logging.debug("Quitting normally")
+      # close the pool. this prevents any more tasks from being submitted.
+      p.close()
+      p.join() # wait for all workers to finish their tasks
+
+    # log the test results
+    logging.info(' PASSED count: %d' % passed)
+    logging.info(' FAILED count: %d' % failed)
+    logging.info(' TIMEOUT count: %d' % timeouts)
+    logging.info(' UNKNOWN count: %d' % unknowns)
+
+if __name__=="__main__":
+    main()
