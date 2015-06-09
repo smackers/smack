31b53cf41ce685a52dc4bd5836a8893c4679b378
Monty Carter <montgomery.carter@gmail.com>
2015-06-09 13:00:04 -0600

    Improve test-bpl.sh reporting.

--- bin/test-bpl.sh
+++ bin/test-bpl.sh
@@ -1,17 +1,47 @@
+GOOD=""
+BAD=""
+
+TOTALCOUNT=0
+for i in `cat $1`
+do
+    for j in `echo $i`
+    do
+	TOTALCOUNT=`expr ${TOTALCOUNT} + 1`
+    done
+done
+CURCOUNT=0
+echo
 for i in `cat $1`
 do
   for j in `echo $i`
   do
-  echo "============================================================================================="
-  echo $j
-  echo "============================================================================================="
     dir=`echo $j | cut -d "/" -f1`
     suffix="."`echo $j |awk -F . '{print $NF}'`
     filename=`basename $j $suffix`
     bplfile=$dir"/"$filename".bpl"
-    bcfile=$dir"/"$filename".bc"
-    smack-boogie.py --verifier=boogie $j -o $bplfile --bc $bcfile
-    #echo $bplfile
-    #echo $bcfile
+    OUT=$(smack.py --no-verify --svcomp $j --bpl $bplfile | grep "SMACK generated invalid Boogie file")
+    if [[ -z $OUT ]] 
+    then
+	GOOD="$j\n${GOOD}"
+    else
+	BAD="$j\n${BAD}"
+    fi
+    CURCOUNT=`expr ${CURCOUNT} + 1`
+    GOODCOUNT=`echo -e "${GOOD}" | wc -l`
+    BADCOUNT=`echo -e "${BAD}" | wc -l`
+    GOODCOUNT=`expr ${GOODCOUNT} - 1`
+    BADCOUNT=`expr ${BADCOUNT} - 1`
+    echo -ne "Completed: ${CURCOUNT} of ${TOTALCOUNT} (Valid: ${GOODCOUNT}, Invalid: ${BADCOUNT})\r"
   done
 done
+echo
+
+echo 
+echo "============================================================================================="
+echo Valid bpl files: ${GOODCOUNT}
+echo "============================================================================================="
+echo -e ${GOOD}
+echo "============================================================================================="
+echo Invalid bpl files: ${BADCOUNT}
+echo "============================================================================================="
+echo -e ${BAD}
