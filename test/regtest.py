2037d7b92afc3e237d3d238bbc35faff3525bae2
Jonathan Whitaker <jon.b.whitaker@gmail.com>
2015-07-31 19:34:57 -0600

    Fixed indentation in the regtest.py script.

--- test/regtest.py
+++ test/regtest.py
@@ -242,7 +242,7 @@ def main():
     # if there are any failed tests or tests that timed out, set the system
     # exit code to a failure status
     if timeouts > 0 or failed > 0:
-        sys.exit(1)
+       sys.exit(1)
 
 if __name__=="__main__":
     main()
