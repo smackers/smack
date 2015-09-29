75c29106ec055e2fe0fc86bef939b335e48cd011
Michael Emmi <michael.emmi@gmail.com>
2015-09-29 23:01:58 +0200

    Only auto-including <smack.h> for svcomp.

--- bin/smack.py
+++ bin/smack.py
@@ -247,8 +247,8 @@ def smack_lib():
 
 def default_clang_compile_command(args):
   cmd = ['clang', '-c', '-emit-llvm', '-O0', '-g', '-gcolumn-info']
+  cmd += ['-I' + smack_headers()]
   cmd += args.clang_options.split()
-  cmd += ['-I' + smack_headers(), '-include' + 'smack.h']
   cmd += ['-DMEMORY_MODEL_' + args.mem_mod.upper().replace('-','_')]
   return cmd
 
@@ -317,6 +317,7 @@ def svcomp_frontend(args):
   name = os.path.splitext(os.path.basename(args.input_files[0]))[0]
 
   args.clang_options += " -DCUSTOM_VERIFIER_ASSERT"
+  args.clang_options += " -include smack.h"
 
   if os.path.splitext(args.input_files[0])[1] == ".i":
     # Ensure clang runs the preprocessor, even with .i extension.
