627147b5bca2bb1b5eb1a0b48ba18df78c0faf53
Zvonimir <zvonimir@cs.utah.edu>
2015-11-02 10:15:53 -0700

    Fixed SMACK wrapper script for SVCOMP

--- bin/smack.sh
+++ bin/smack.sh
@@ -4,8 +4,8 @@ ROOT="$( cd "${SMACK_BIN}" && cd .. && cd .. && pwd )"
 CORRAL_DIR="${ROOT}/corral"
 LLVM_DIR="${ROOT}/llvm"
 
-export PATH=${LLVM_DIR}/bin:$PATH
+export PATH=${LLVM_DIR}/bin:$SMACK_BIN:$PATH
 export CORRAL="mono ${CORRAL_DIR}/bin/Release/corral.exe"
 
-$SMACK_BIN/smack.py -x=svcomp --verifier=svcomp -q $@
+smack.py -x=svcomp --verifier=svcomp -q $@
 
