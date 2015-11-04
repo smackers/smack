eac26f5c865da96e85f309eafaaada6daf265f26
Zvonimir <zvonimir@cs.utah.edu>
2015-11-04 10:19:24 -0700

    Added lockpwn environment variable to SMACK bash script

--- bin/smack.sh
+++ bin/smack.sh
@@ -2,10 +2,12 @@
 SMACK_BIN="$( cd "$(dirname "$(readlink -f "${0}")")" && pwd )"
 ROOT="$( cd "${SMACK_BIN}" && cd .. && cd .. && pwd )"
 CORRAL_DIR="${ROOT}/corral"
+LOCKPWN_DIR="${ROOT}/lockpwn"
 LLVM_DIR="${ROOT}/llvm"
 
 export PATH=${LLVM_DIR}/bin:$SMACK_BIN:$PATH
 export CORRAL="mono ${CORRAL_DIR}/bin/Release/corral.exe"
+export LOCKPWN="mono ${LOCKPWN_DIR}/Binaries/lockpwn.exe"
 
 smack.py -x=svcomp --verifier=svcomp -q $@
 
