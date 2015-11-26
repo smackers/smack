37237246ae7f717689050a4d3d41269e3068ea4d
Zvonimir <zvonimir@cs.utah.edu>
2015-11-25 17:41:26 -0700

    Created a folder for failing regressions

---
+++ test/failing/struct_by_value.c
@@ -0,0 +1,22 @@
+#include "smack.h"
+
+// @expect verified
+
+struct S {
+  int x;
+  int y;
+};
+
+int eq(struct S p1, struct S p2) {
+  return p1.y == p2.y;
+}
+
+int main(void) {
+  struct S p = {0, 0};
+  struct S q = {1, 1};
+  p = q;
+  assert(p.y == q.y);
+  assert(eq(p,q));
+  return 0;
+}
+
