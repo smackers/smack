14ad0e49d1a8aa33cd19dec18098b349d9f06133
Zvonimir <zvonimir@cs.utah.edu>
2015-11-28 09:16:15 -0700

    Setting mono heap size only in the SVCOMP script

--- bin/smack-svcomp-wrapper.sh
+++ bin/smack-svcomp-wrapper.sh
@@ -8,8 +8,12 @@ CORRAL_BIN="${ROOT}/corral"
 LOCKPWN_BIN="${ROOT}/lockpwn"
 LLVM_BIN="${ROOT}/llvm/bin"
 
+# Setting mono heap size to 9GB
+export MONO_GC_PARAMS=max-heap-size=9g
+
 export PATH=${LLVM_BIN}:$SMACK_BIN:$PATH
 export CORRAL="mono ${CORRAL_BIN}/corral.exe"
 export LOCKPWN="mono ${LOCKPWN_BIN}/lockpwn.exe"
 
 smack -x=svcomp --verifier=svcomp -q $@
+
