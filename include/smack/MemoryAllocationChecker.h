95302dfe8cb2ab4a47a3f095b78ab964a32ef1f2
Jiten <jitenmt@gmail.com>
2016-02-19 17:59:44 +0000

    Some more cleanup

--- include/smack/MemoryAllocationChecker.h
+++ include/smack/MemoryAllocationChecker.h
@@ -8,11 +8,8 @@
 
 namespace smack {
   class MemoryAllocationChecker: public llvm::ModulePass {
-    //private:
-      //llvm::raw_ostream &out;
     public:
       static char ID; // Pass identification, replacement for typeid
-      //MemoryAllocationChecker(llvm::raw_ostream &out) : llvm::ModulePass(ID), out(out) {}
       MemoryAllocationChecker() : llvm::ModulePass(ID) {}
       virtual bool runOnModule(llvm::Module& m);
   };
