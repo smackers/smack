bc55e57da14af040377c69b46d13002cf512ed9e
Jiten <jitenmt@gmail.com>
2016-03-06 19:21:11 +0000

    added memory-safety check as an option. Disabled by default

--- tools/llvm2bpl/llvm2bpl.cpp
+++ tools/llvm2bpl/llvm2bpl.cpp
@@ -53,6 +53,10 @@ static llvm::cl::opt<std::string>
 DefaultDataLayout("default-data-layout", llvm::cl::desc("data layout string to use if not specified by module"),
   llvm::cl::init(""), llvm::cl::value_desc("layout-string"));
 
+static llvm::cl::opt<bool>
+MemorySafety("memory-safety", llvm::cl::desc("Enable memory safety checks"),
+  llvm::cl::init(false));
+
 std::string filenamePrefix(const std::string &str) {
   return str.substr(0, str.find_last_of("."));
 }
@@ -123,7 +127,10 @@ int main(int argc, char **argv) {
   pass_manager.add(new llvm::MergeArrayGEP());
   // pass_manager.add(new smack::SimplifyLibCalls());
   pass_manager.add(new llvm::Devirtualize());
-  pass_manager.add(new smack::MemorySafetyChecker());
+   
+  if(MemorySafety) {
+    pass_manager.add(new smack::MemorySafetyChecker());
+  }
 
   std::vector<tool_output_file*> files;
 
