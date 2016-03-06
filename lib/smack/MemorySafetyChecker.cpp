2ae60923cc8d8442159e66d74144294235fbaa43
Jiten <jitenmt@gmail.com>
2016-03-06 16:42:09 +0000

    Directly iterating over instructions from function instead of going through basic blocks

--- lib/smack/MemorySafetyChecker.cpp
+++ lib/smack/MemorySafetyChecker.cpp
@@ -6,6 +6,7 @@
 
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Instructions.h"
+#include "llvm/IR/InstIterator.h"
 #include "llvm/IR/Constants.h"
 #include "llvm/Support/Debug.h"
 
@@ -17,23 +18,21 @@ namespace smack {
     Function* verifierFunction = m.getFunction(Naming::VERIFIER_FUNCTION);
     for (auto& F : m) {
       if(!Naming::isSmackName(F.getName())) {
-        for (auto& B : F) {
-          for (auto& I : B) {
-            if(LoadInst* li = dyn_cast<LoadInst>(&I)) {
-              Value* pointer = li->getPointerOperand();
-              Value* size = ConstantInt::get(IntegerType::get(F.getContext(), 64), dataLayout->getTypeSizeInBits(dyn_cast<PointerType>(li->getPointerOperand()->getType())->getPointerElementType()), false);
-              Type *PtrTy = PointerType::getUnqual(IntegerType::getInt8Ty(F.getContext()));
-              CastInst* castPointer = CastInst::Create(Instruction::BitCast, pointer, PtrTy, "", &I);
-              Value* args[] = {castPointer, size};
-              CallInst::Create(verifierFunction, ArrayRef<Value*>(args, 2), "", &I);
-            } else if (StoreInst* si = dyn_cast<StoreInst>(&I)) {
-              Value* pointer = si->getPointerOperand();
-              Value* size = ConstantInt::get(IntegerType::get(F.getContext(), 64), dataLayout->getTypeSizeInBits(si->getValueOperand()->getType()), false);
-              Type *PtrTy = PointerType::getUnqual(IntegerType::getInt8Ty(F.getContext()));
-              CastInst* castPointer = CastInst::Create(Instruction::BitCast, pointer, PtrTy, "", &I);
-              Value* args[] = {castPointer, size};
-              CallInst::Create(verifierFunction, ArrayRef<Value*>(args, 2), "", &I);
-            }
+        for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I) {
+          if(LoadInst* li = dyn_cast<LoadInst>(&*I)) {
+            Value* pointer = li->getPointerOperand();
+            Value* size = ConstantInt::get(IntegerType::get(F.getContext(), 64), dataLayout->getTypeSizeInBits(dyn_cast<PointerType>(li->getPointerOperand()->getType())->getPointerElementType()), false);
+            Type *PtrTy = PointerType::getUnqual(IntegerType::getInt8Ty(F.getContext()));
+            CastInst* castPointer = CastInst::Create(Instruction::BitCast, pointer, PtrTy, "", &*I);
+            Value* args[] = {castPointer, size};
+            CallInst::Create(verifierFunction, ArrayRef<Value*>(args, 2), "", &*I);
+          } else if (StoreInst* si = dyn_cast<StoreInst>(&*I)) {
+            Value* pointer = si->getPointerOperand();
+            Value* size = ConstantInt::get(IntegerType::get(F.getContext(), 64), dataLayout->getTypeSizeInBits(si->getValueOperand()->getType()), false);
+            Type *PtrTy = PointerType::getUnqual(IntegerType::getInt8Ty(F.getContext()));
+            CastInst* castPointer = CastInst::Create(Instruction::BitCast, pointer, PtrTy, "", &*I);
+            Value* args[] = {castPointer, size};
+            CallInst::Create(verifierFunction, ArrayRef<Value*>(args, 2), "", &*I);
           }
         }
       }
