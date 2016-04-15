e9bc1a7ab0a2b44f3fff6b3c89ffa50417163aab
Michael Quigley <mquigley@cs.utah.edu>
2016-04-15 01:35:39 -0600

    -Update tests to include division. -Note: It looks like intrinsics aren't used for division.

--- test/rust/mul_test32.rs
+++ test/rust/mul_test32.rs
@@ -48,7 +48,27 @@ fn main() {
         let y: u32 = 4;
         assert!(x+y != 8);
     }
-
+    //division
+    {    
+        let x: u32 = 8;
+        let y: u32 = 4;
+        assert!(x/y == 2);
+    }
+    {    
+        let x: u32 = 8;
+        let y: u32 = 4;
+        assert!(x/y != 3);
+    }
+    {    
+        let x: i32 = 8;
+        let y: i32 = 4;
+        assert!(x/y == 2);
+    }
+    {    
+        let x: i32 = 8;
+        let y: i32 = 4;
+        assert!(x/y != 3);
+    }
 
     
 }
