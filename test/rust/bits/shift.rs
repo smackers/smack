1f42a4b90135989794352b18648556a9fbec718d
Michael Quigley <mquigley@cs.utah.edu>
2016-04-19 21:34:39 -0600

    Removed macro definitions from bits/shift.rs test

--- test/rust/bits/shift.rs
+++ test/rust/bits/shift.rs
@@ -1,11 +1,3 @@
-extern {fn __VERIFIER_assert(x: i32);}
-
-macro_rules! assert {
-    ( $cond:expr ) => (
-        unsafe { __VERIFIER_assert($cond as i32); }
-    );
-}
-
 fn main() {
     // Shift right
     {
