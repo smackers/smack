c5f600b8bd6abc39aa438021b7c0a978cbfc457d
Zvonimir <zvonimir@cs.utah.edu>
2016-04-23 13:20:26 -0600

    Fixed init func examples to work with bit-precise
    One has to be careful when using SMACK_code functionality to
    ensure portability when bit-precise is used. I slightly tweaked
    the type of the introduced ghost array to ensure portability.

--- test/basic/init_funcs_example_fail.c
+++ test/basic/init_funcs_example_fail.c
@@ -20,18 +20,18 @@ __SMACK_INIT(defineProcessStates) {
   __SMACK_top_decl("const unique $process_running: int;");
   __SMACK_top_decl("const unique $process_waiting: int;");
   __SMACK_top_decl("const unique $process_stopped: int;");
-  __SMACK_top_decl("var $processStatus: [int]int;");
+  __SMACK_top_decl("var $processStatus: [ref]int;");
 }
 
 // This is the one that would need to be in a __SMACK_INIT call,
 // since this assumption is not just a declaration/definition.
 // This definition would also be listed in the model's header file.
 __SMACK_INIT(initializeProcessStates) {
-  __SMACK_code("assume (forall x:int :: $processStatus[x] == $process_uninitialized);");
+  __SMACK_code("assume (forall x:ref :: $processStatus[x] == $process_uninitialized);");
 }
 
 int main(void) {
-  int idx = __VERIFIER_nondet_int();
+  void* idx = __VERIFIER_nondet_pointer();
   __SMACK_code("assert($processStatus[@] != $process_uninitialized);", idx);
 }
 
