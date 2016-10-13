4960c12c51f66153c79aef1d3cd9dc207e45c9a5
Zvonimir <zvonimir@cs.utah.edu>
2016-10-12 19:42:28 -0600

    Updated SMACK version number on SVCOMP script

--- svcomp/bench/src/benchexec/benchexec/tools/smack_benchexec_driver.py
+++ svcomp/bench/src/benchexec/benchexec/tools/smack_benchexec_driver.py
@@ -35,7 +35,7 @@ class Tool(benchexec.tools.template.BaseTool):
         Sets the version number for SMACK, which gets displayed in the "Tool" row
         in BenchExec table headers.
         """
-        return '1.5.1dev'
+        return '1.6.0'
 
     def name(self):
         """
