d83ac356dc12e9687abbc5d1ab92fb3a5b21c6fc
Checkmate50 <dgeisler50@gmail.com>
2016-11-06 13:40:14 -0700

    Fixed a typo

--- lib/smack/SmackRep.cpp
+++ lib/smack/SmackRep.cpp
@@ -504,7 +504,7 @@ const Expr* SmackRep::pointerToInteger(const Expr* e, unsigned width) {
 
 const Expr* SmackRep::integerToPointer(const Expr* e, unsigned width) {
   if (width < ptrSizeInBits)
-    e = (opName("$zext", {width, ptrSizeInBits}), e);
+    e = Expr::fn(opName("$zext", {width, ptrSizeInBits}), e);
   else if (width > ptrSizeInBits)
     e = Expr::fn(opName("$trunc", {width, ptrSizeInBits}), e);
   e = bitConversion(e, SmackOptions::BitPrecise, SmackOptions::BitPrecisePointers);
