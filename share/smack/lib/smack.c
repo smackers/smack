cf3f71a5db7b8ec2281b9656370ab1cc112a036c
Mark S. Baranowski (Marek Stanisław Baranowski) <keram88@users.noreply.github.com>
2017-01-05 14:41:40 -0700

    Update smack.c

--- share/smack/lib/smack.c
+++ share/smack/lib/smack.c
@@ -98,6 +98,7 @@ void* realloc(void* ptr, unsigned long size) {
     // Overapproximate the behavior of realloc
     ret = malloc(size);
     memcpy(ret, ptr, size);
+    free(ptr);
   }
   return ret;
 }
