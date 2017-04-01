e62eb13d7ebafd11f7b79e821d8a7766aab5bd7f
Michael Emmi <michael.emmi@gmail.com>
2017-03-31 21:05:57 -0400

    Basic replay functionality.

--- share/smack/replay.py
+++ share/smack/replay.py
@@ -1,2 +1,86 @@
-def replay_error_trace(verifier_output):
-    print "TODO: replay the erorr trace"
+import os
+import subprocess
+import sys
+from utils import temporary_file, try_command
+
+STUBBABLE_FUNCTIONS = [
+  "__VERIFIER_nondet"
+]
+
+def replay_error_trace(verifier_output, args):
+  read, write = os.pipe()
+  os.write(write, verifier_output)
+  os.close(write)
+  with open(args.replay_harness, 'w') as f:
+    f.write(stub_module(aggregate_values(collect_returns(read))))
+
+  stubs_bc = temporary_file('stubs', '.bc', args)
+  try_command(['clang', '-c', '-emit-llvm', '-o', stubs_bc, args.replay_harness])
+  try_command(['clang', '-o', args.replay_exe_file, args.bc_file, stubs_bc])
+  print "Generated replay harness:", args.replay_harness
+  print "Generated replay executable:", args.replay_exe_file
+
+  if try_command(["./" + args.replay_exe_file]):
+    print "Error-trace replay successful."
+
+  else:
+    print "Error-trace replay failed."
+
+
+def collect_returns(stream):
+  return reduce(
+  (lambda s, c: subprocess.Popen(c, stdin=s, stdout=subprocess.PIPE).stdout), [
+    ["grep", "-A", "1", "-e", "RETURN from \(%s\)" % "\|".join(STUBBABLE_FUNCTIONS)],
+    ["sed", "s/.*  (\(.*\))/\\1/"],
+    ["cut", "-d", " ", "-f3"],
+    ["grep", "-v", "-e", "--"],
+    ["paste", "-d,", "-", "-"]
+  ], stream)
+
+
+def aggregate_values(stream):
+  dict = {}
+  for line in stream:
+    fn, val = line.strip().split(",")
+    if not fn in dict:
+        dict[fn] = []
+    dict[fn].append(val)
+  return dict
+
+
+def stub_module(dict):
+  code = []
+  code.append("""//
+// This file was automatically generated from a Boogie error trace.
+// It contains stubs for unspecified functions that were called in that trace.
+// These stubs will return the same values they did in the error trace.
+// This file can be linked with the source program to reproduce the error.
+//
+#include <stdlib.h>
+#include <stdio.h>
+
+void __VERIFIER_assert(int b) {
+  if (!b)
+    printf("error reached!\\n");
+    exit(0);
+}
+
+void __VERIFIER_assume(int b) {
+  if (!b) {
+    printf("assumption does not hold.\\n");
+    exit(-1);
+  }
+}
+""")
+
+  for fn, vals in dict.items():
+    code.append("""// stub for function: %(fn)s
+int %(fn)s$table[] = {%(vals)s};
+int %(fn)s$idx = 0;
+
+int %(fn)s() {
+  return %(fn)s$table[%(fn)s$idx++];
+}
+""" % {'fn': fn, 'vals': ", ".join(vals)})
+
+  return "\n".join(code)
---
+++ share/smack/utils.py
@@ -0,0 +1,80 @@
+import os
+import sys
+import tempfile
+import subprocess
+import signal
+from threading import Timer
+import top
+
+temporary_files = []
+
+def temporary_file(prefix, extension, args):
+  f, name = tempfile.mkstemp(extension, prefix + '-', os.getcwd(), True)
+  os.close(f)
+  if not args.debug:
+    temporary_files.append(name)
+  return name
+
+def remove_temp_files():
+  for f in temporary_files:
+    if os.path.isfile(f):
+      os.unlink(f)
+
+def timeout_killer(proc, timed_out):
+  if not timed_out[0]:
+    timed_out[0] = True
+    os.killpg(os.getpgid(proc.pid), signal.SIGKILL)
+
+def try_command(cmd, cwd=None, console=False, timeout=None):
+  args = top.args
+  console = (console or args.verbose or args.debug) and not args.quiet
+  filelog = args.debug
+  output = ''
+  proc = None
+  timer = None
+  try:
+    if args.debug:
+      print "Running %s" % " ".join(cmd)
+
+    proc = subprocess.Popen(cmd, cwd=cwd, preexec_fn=os.setsid,
+      stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    if timeout:
+      timed_out = [False]
+      timer = Timer(timeout, timeout_killer, [proc, timed_out])
+      timer.start()
+
+    if console:
+      while True:
+        line = proc.stdout.readline()
+        if line:
+          output += line
+          print line,
+        elif proc.poll() is not None:
+          break
+      proc.wait
+    else:
+      output = proc.communicate()[0]
+
+    if timeout:
+      timer.cancel()
+
+    rc = proc.returncode
+    proc = None
+    if timeout and timed_out[0]:
+      return output + ("\n%s timed out." % cmd[0])
+    elif rc:
+      raise RuntimeError("%s returned non-zero." % cmd[0])
+    else:
+      return output
+
+  except (RuntimeError, OSError) as err:
+    print >> sys.stderr, output
+    sys.exit("Error invoking command:\n%s\n%s" % (" ".join(cmd), err))
+
+  finally:
+    if timeout and timer: timer.cancel()
+    if proc: os.killpg(os.getpgid(proc.pid), signal.SIGKILL)
+    if filelog:
+      with open(temporary_file(cmd[0], '.log', args), 'w') as f:
+        f.write(output)
