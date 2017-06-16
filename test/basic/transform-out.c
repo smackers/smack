7af484272a097a6041628f83d056f69a0b54fc30
Michael Emmi <michael.emmi@gmail.com>
2017-06-16 19:18:17 -0400

    Fixed Boogie result transform regression.

--- test/basic/transform-out.c
+++ test/basic/transform-out.c
@@ -3,7 +3,7 @@
 #include "smack.h"
 
 // @expect verified
-// @flag --transform-out "sed -e 's/\d* verified, \d* errors?/1 verified, 0 errors/' -e 's/can fail/no bugs/'"
+// @flag --transform-out "sed -e 's/[[:digit:]]* verified, [[:digit:]]* error/1 verified, 0 errors/' -e 's/can fail/no bugs/'"
 
 int main(void) {
   assert (0);
