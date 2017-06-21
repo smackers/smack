9b88e1292ee3590a56a84da46c6ad71151901bb6
Michael Emmi <michael.emmi@gmail.com>
2017-06-20 22:17:55 -0400

    Renamed module debug identifier.

--- lib/smack/VerifierCodeMetadata.cpp
+++ lib/smack/VerifierCodeMetadata.cpp
@@ -2,7 +2,7 @@
 // This file is distributed under the MIT License. See LICENSE for details.
 //
 
-#define DEBUG_TYPE "contracts"
+#define DEBUG_TYPE "verifier-code-metadata"
 
 #include "smack/SmackOptions.h"
 #include "smack/Naming.h"
