54ff4d75af83baf8de62292e4d9d9c2f0830ff8d
Checkmate50 <dgeisler50@gmail.com>
2017-06-26 17:13:58 -0600

    Updated squareroot tests

--- test/float/squareroot_fail.c
+++ test/float/squareroot_fail.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include "smack.h"
 
-// @expect verified
+// @expect error
 
 int main() {
   double a;
   double b;
-
-  a = sqrt(4.0);
+  
+  a = sqrt(4.01);
   b = 2.0;
-
+  
   assert(a == b);
 }
