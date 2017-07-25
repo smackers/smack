021063b7046744f3b7be6e1380c4dabbfbaac6f0
Shaobo <polarishehn@gmail.com>
2017-07-24 18:39:27 -0600

    Added two regression tests for the flag `--split-structs`

---
+++ test/basic/split_structs.c
@@ -0,0 +1,27 @@
+#include "smack.h"
+
+//There exists a load/store instruction of type {i64, i32}
+//in function main, respectively.
+
+// @flag --split-structs
+// @expect verified
+
+typedef struct {
+  int x;
+  int y;
+  int z;
+} S;
+
+S foo() {
+  S s;
+  s.x = 1;
+  s.y = 2;
+  s.z = 3;
+  return s;
+}
+
+int main() {
+  S s = foo();
+  assert(s.z == 3);
+  return 0;
+}
