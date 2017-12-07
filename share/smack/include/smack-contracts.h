84e7662ffa7e026bdf9747220532eed19b411f7c
Michael Emmi <michael.emmi@gmail.com>
2017-12-07 10:50:33 -0500

    Updated contract header to disable quantifiers.

--- share/smack/include/smack-contracts.h
+++ share/smack/include/smack-contracts.h
@@ -9,18 +9,27 @@
 void __CONTRACT_requires(bool expr);
 void __CONTRACT_ensures(bool expr);
 void __CONTRACT_invariant(bool expr);
-int  __CONTRACT_int_variable(const char *name) __attribute__((const));
-bool __CONTRACT_forall(const char *var, bool expr) __attribute__((const));
-bool __CONTRACT_exists(const char *var, bool expr) __attribute__((const));
 
 #define requires(X) __CONTRACT_requires(X)
 #define ensures(X) __CONTRACT_ensures(X)
 #define invariant(X) __CONTRACT_invariant(X)
-#define qvar(X,T) __CONTRACT_ ## T ## _variable(X)
-#define forall(X,Y) __CONTRACT_forall(X,Y)
-#define exists(X,Y) __CONTRACT_exists(X,Y)
 
-int old(int term);
-int result(void);
+// TODO provide quantifier bound variables
+// int  __CONTRACT_int_variable(const char *name) __attribute__((const));
+// #define qvar(X,T) __CONTRACT_ ## T ## _variable(X)
+
+// TODO provide universal quantifier
+// bool __CONTRACT_forall(const char *var, bool expr) __attribute__((const));
+// #define forall(X,Y) __CONTRACT_forall(X,Y)
+
+// TODO provide existential quantifier
+// bool __CONTRACT_exists(const char *var, bool expr) __attribute__((const));
+// #define exists(X,Y) __CONTRACT_exists(X,Y)
+
+// TODO provide precondition state “old” expressions
+// int old(int term);
+
+// TODO provide procedure “result” expression
+// int result(void);
 
 #endif
