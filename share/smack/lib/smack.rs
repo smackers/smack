5895a985db380927a76201593e87a7c2cd88a971
Mark S. Baranowski <mark.s.baranowski@gmail.com>
2018-01-03 15:38:12 -0700

    Indentation

--- share/smack/lib/smack.rs
+++ share/smack/lib/smack.rs
@@ -2,13 +2,13 @@ extern {fn __VERIFIER_assert(x: i32);}
 
 #[macro_export]
 macro_rules! assert {
-    ( $cond:expr ) => ( unsafe {__VERIFIER_assert($cond as i32);} )
+  ( $cond:expr ) => ( unsafe {__VERIFIER_assert($cond as i32);} )
 }
 
 
 #[macro_export]
 macro_rules! assert_eq {
-    ( $lhs:expr, $rhs:expr ) => ( assert!($lhs == $rhs); )
+  ( $lhs:expr, $rhs:expr ) => ( assert!($lhs == $rhs); )
 }
 
 
