81ee72ddb12c2531210bf530bf47e4dd55d252a1
Mark S. Baranowski <mark.s.baranowski@gmail.com>
2018-01-19 22:40:04 -0700

    Structures

---
+++ examples/rust/structures/option.rs
@@ -0,0 +1,21 @@
+fn safe_div(x: u64, y: u64) -> Option<u64> {
+  if y != 0 {
+    Some(x/y)
+  }
+  else {
+    None
+  }
+}
+
+fn main() {
+  let a = safe_div(4,2);
+  match a {
+    Some(x) => assert!(x == 2),
+    None => assert!(false)
+  };
+  let b = safe_div(4,0);
+  match b {
+    Some(x) => assert!(false),
+    None => assert!(true)
+  };
+}
---
+++ examples/rust/structures/point_fail.rs
@@ -0,0 +1,40 @@
+use std::ops::{Add, AddAssign};
+
+#[derive(PartialEq,Clone,Copy)]
+struct Point {
+  x: u64,
+  y: u64
+}
+
+impl Point {
+  pub fn new(_x: u64, _y: u64) -> Point {
+    Point { x: _x, y: _y }
+  }
+  pub fn get_x(self) -> u64 {
+    self.x
+  }
+  pub fn get_y(self) -> u64 {
+    self.y
+  }
+}
+
+impl Add for Point {
+  type Output = Point;
+  fn add(self, other: Point) -> Point {
+    Point::new(self.x + other.x, self.y + other.y)
+  }
+}
+
+impl AddAssign for Point {
+  fn add_assign(&mut self, other: Point) {
+    self.x += other.x;
+    self.y += other.y;
+  }
+}
+
+fn main() {
+  let a = Point::new(1,2);
+  let b = Point::new(3,4);
+  let c = a + b;
+  assert!(c == Point::new(3,6));
+}
