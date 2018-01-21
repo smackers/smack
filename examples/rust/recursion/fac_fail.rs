d4a8084023b8ef46d736e51ab7c57190822f189b
Mark S. Baranowski <mark.s.baranowski@gmail.com>
2018-01-20 20:22:12 -0700

    Recursion examples

---
+++ examples/rust/recursion/fac_fail.rs
@@ -0,0 +1,11 @@
+fn fac(n: u64, acc: u64) -> u64 {
+  match n {
+    0 => acc,
+    _ => fac(n - 1, acc * n)
+  }
+}
+
+fn main() {
+   let x = fac(5, 1);
+   assert!(x != 120);
+}
---
+++ examples/rust/recursion/fib_fail.rs
@@ -0,0 +1,12 @@
+fn fib(x: u64) -> u64 {
+  match x {
+    0 => 1,
+    1 => 1,
+    _ => fib(x-1) + fib(x-2)
+  }
+}
+
+fn main() {
+  let x = fib(6);
+  assert!(x != 13);
+}
