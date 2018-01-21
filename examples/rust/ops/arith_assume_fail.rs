d2279dc4321d7e9ed5a0fa29d6509271d1531470
Mark S. Baranowski <mark.s.baranowski@gmail.com>
2018-01-20 19:42:17 -0700

    Another nondet example

---
+++ examples/rust/ops/arith_assume_fail.rs
@@ -0,0 +1,10 @@
+fn main() {
+  let a = 6i32.nondet();
+  let b = 7i32.nondet();
+  assume!(4 < a && a < 8); // a in [5,7]
+  assume!(5 < b && b < 9); // b in [6,8]
+  let x = a * b;
+  assert!(!(x==30 || x==35 || x==40 || 
+            x==36 || x==48 || x==42 || 
+            x==49 || x==56)); // a*b != anythin allowed
+}
