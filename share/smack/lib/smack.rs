0cf05dbd237e77c305d6ba3a62f0f4997a91a5e2
Mark S. Baranowski <mark.s.baranowski@gmail.com>
2018-01-20 19:00:41 -0700

    Add nondet functionality

--- share/smack/lib/smack.rs
+++ share/smack/lib/smack.rs
@@ -1,14 +1,78 @@
-extern {fn __VERIFIER_assert(x: i32);}
+#[cfg(verifier = "smack")]
+extern {
+  fn __VERIFIER_assert(x: i32);
+  fn __VERIFIER_assume(x: i32);
+  fn __VERIFIER_nondet_signed_char() -> i8;
+  fn __VERIFIER_nondet_unsigned_char() -> u8;
+  fn __VERIFIER_nondet_signed_short() -> i16;
+  fn __VERIFIER_nondet_unsigned_short() -> u16;
+  fn __VERIFIER_nondet_signed_int() -> i32;
+  fn __VERIFIER_nondet_unsigned_int() -> u32;
+  fn __VERIFIER_nondet_signed_long_long() -> i64;
+  fn __VERIFIER_nondet_unsigned_long_long() -> u64;
 
-#[macro_export]
-macro_rules! assert {
-  ( $cond:expr ) => ( unsafe {__VERIFIER_assert($cond as i32);} )
 }
 
 
+#[cfg(verifier = "smack")]
+#[macro_export]
+macro_rules! assert {
+  ( $cond:expr ) =>
+    (
+      #[cfg(verifier = "smack")]
+      unsafe { __VERIFIER_assert($cond as i32); }
+    )
+}
+
+#[cfg(verifier = "smack")]
 #[macro_export]
 macro_rules! assert_eq {
   ( $lhs:expr, $rhs:expr ) => ( assert!($lhs == $rhs); )
 }
 
+#[cfg(verifier = "smack")]
+#[macro_export]
+macro_rules! assume {
+  ( $cond:expr ) =>
+    (
+      unsafe { __VERIFIER_assume($cond as i32); }     
+    )
+}
 
+#[macro_export]
+macro_rules! nondet {
+  ($e:expr) =>
+  { $e }
+}
+
+trait NonDet {
+  fn nondet(self) -> Self;
+}
+
+#[macro_export]
+macro_rules! make_nondet {
+  ($typ:ident, $nondet:ident) =>
+    (
+      impl NonDet for $typ {
+        #[cfg(verifier = "smack")]
+        fn nondet(self) -> Self {
+          unsafe { $nondet() as Self }
+        }
+        
+        #[cfg(not(verifier = "smack"))]
+        fn nondet(self) -> Self {
+          val
+        }            
+      }
+    );
+}
+
+/* Instantiate nondet for all integer types. */
+make_nondet!(i8, __VERIFIER_nondet_signed_char);
+make_nondet!(u8, __VERIFIER_nondet_unsigned_char);
+make_nondet!(i16, __VERIFIER_nondet_signed_short);
+make_nondet!(u16, __VERIFIER_nondet_unsigned_short);
+make_nondet!(i32, __VERIFIER_nondet_signed_int);
+make_nondet!(u32, __VERIFIER_nondet_unsigned_int);
+make_nondet!(i64, __VERIFIER_nondet_signed_long_long);
+make_nondet!(u64, __VERIFIER_nondet_unsigned_long_long);
