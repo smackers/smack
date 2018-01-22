abefe00e88eec4c695f0a1cce518ba758e0410fc
Mark S. Baranowski <mark.s.baranowski@gmail.com>
2018-01-21 19:33:04 -0700

    Generic functions and structure

---
+++ examples/rust/generics/generic_function.rs
@@ -0,0 +1,34 @@
+struct Point {
+  pub x:u64,
+  pub y:u64
+}
+
+trait D {
+  fn double(self) -> Self;
+}
+
+impl D for u8 {
+  fn double(self) -> Self {
+    2*self
+  }
+}
+
+impl D for Point {
+  fn double(self) -> Self {
+    Point { x: 2*self.x, y: 2*self.y }
+  }
+}
+
+fn double<T: D>(x: T) -> T {
+  x.double()
+}
+
+fn main() {
+  let a:u8 = 17;
+  let p = Point { x: 3, y: 4 };
+  let b = double(a);
+  let q = double(p);
+  assert!(b == 34);
+  assert!(q.x == 6);
+  assert!(q.y == 8);
+}
---
+++ examples/rust/generics/generic_point.rs
@@ -0,0 +1,11 @@
+struct Point<T> {
+  pub x: T,
+  pub y: T
+}
+
+fn main() {
+  let a:Point<u8> = Point { x: 7, y: 8 };
+  let b:Point<u64> = Point { x: 7, y: 8 };
+  assert!(a.x as u64 == b.x);
+  assert!(a.y as u64 == b.y);
+}
