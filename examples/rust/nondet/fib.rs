1b0dd29a338831450e3c5d9244793ce0b37461e0
Mark S. Baranowski <mark.s.baranowski@gmail.com>
2018-01-23 15:51:40 -0700

    Nondet examples

---
+++ examples/rust/nondet/fib.rs
@@ -0,0 +1,11 @@
+fn main() {
+  let (mut last, mut a, mut b) = (0, 0, 1);
+  let c = 6u64.nondet();
+  assume!(c > 0);
+  for _ in 0..c as u64 {
+    last = a;
+    a = b;
+    b = a + last;
+  }
+  assert!(a + last == b);
+}
---
+++ examples/rust/nondet/sum.rs
@@ -0,0 +1,9 @@
+fn main() {
+  let mut sum = 0;
+  let b = 7u64.nondet();
+  assume!(b > 1);
+  for i in 0..b as u64 {
+    sum += i;
+  }
+  assert!(sum == b*(b-1)/2);
+}
