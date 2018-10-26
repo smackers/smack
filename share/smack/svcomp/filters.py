99376b6f3fb32092096c92ab516cc7fbffadeaaf
Zvonimir <zvonimir@cs.utah.edu>
2018-10-26 08:23:02 -0600

    Fixed float filter for SVCOMP

--- share/smack/svcomp/filters.py
+++ share/smack/svcomp/filters.py
@@ -48,7 +48,8 @@ def svcomp_filter(f):
     
 def bv_filter(lines, raw_line_count, pruned_line_count):
   if ("bugBrokenOut" in lines or "returnsStructure" in lines or "__VERIFIER_nondet_double" in lines or
-      "__VERIFIER_nondet_float" in lines or "0x43300000" in lines or "float X, P;" in lines or "1415926538837245" in lines):
+      "__VERIFIER_nondet_float" in lines or "0x43300000" in lines or "float X, P;" in lines or "1415926538837245" in lines or
+      "huge_floor" in lines or "huge_ceil" in lines or "tiny_sqrt" in lines or "fmax_float" in lines):
     return 0
   elif ('4294967294u' in lines or '616783' in lines):
     return 1
