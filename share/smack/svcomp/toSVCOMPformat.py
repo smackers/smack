8f53d4c3eb4ce2b1d0b72fc46347ce2843b25885
Zvonimir <zvonimir@cs.utah.edu>
2018-11-22 07:07:02 -0700

    Minor fix to Python syntax

--- share/smack/svcomp/toSVCOMPformat.py
+++ share/smack/svcomp/toSVCOMPformat.py
@@ -185,9 +185,9 @@ def smackJsonToXmlGraph(strJsonOutput, args, hasBug, status):
               continue
             callStack.append(calledFunc)
             if args.only_check_memcleanup or status == 'invalid-memtrack':
-              if "__SMACK_check_memory_leak" in jsonTrace["description"][len("CALL"):] or
-                 "__VERIFIER_error" in jsonTrace["description"][len("CALL"):] or
-                 "exit" in jsonTrace["description"][len("CALL"):]:
+              if ("__SMACK_check_memory_leak" in jsonTrace["description"][len("CALL"):] or
+                  "__VERIFIER_error" in jsonTrace["description"][len("CALL"):] or
+                  "exit" in jsonTrace["description"][len("CALL"):]):
                 newNode = addGraphNode(tree)
                 # addGraphNode returns a string, so we had to search the graph to get the node that we want
                 vNodes =tree.find("graph").findall("node")
