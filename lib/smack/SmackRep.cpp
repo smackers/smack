63d2a68a5dfb5b0298715512a99e7a46c2609fbd
Zvonimir <zvonimir@cs.utah.edu>
2019-01-18 07:58:11 -0700

    Added i160 integer size
    Fixes #404

--- lib/smack/SmackRep.cpp
+++ lib/smack/SmackRep.cpp
@@ -54,7 +54,7 @@ namespace smack {
 
 const unsigned MEMORY_INTRINSIC_THRESHOLD = 0;
 
-const std::vector<unsigned> INTEGER_SIZES = {1, 5, 6, 8, 16, 24, 32, 40, 48, 56, 64, 80, 88, 96, 128, 256};
+const std::vector<unsigned> INTEGER_SIZES = {1, 5, 6, 8, 16, 24, 32, 40, 48, 56, 64, 80, 88, 96, 128, 160, 256};
 const std::vector<unsigned> REF_CONSTANTS = {
   0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
   1024
