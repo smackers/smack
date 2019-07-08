d3c967764293c07abb5a2f362c0d08c7564195e6
Zvonimir <zvonimir@cs.utah.edu>
2019-07-08 07:37:08 -0600

    Formatted new regressions

--- test/bits/byte_swap.c
+++ test/bits/byte_swap.c
@@ -5,30 +5,30 @@
 
 uint16_t bswap16(uint16_t x) {
   uint16_t result = 0;
-  result |= (x&0x00FF) << 8;
-  result |= (x&0xFF00) >> 8;
+  result |= (x & 0x00FF) << 8;
+  result |= (x & 0xFF00) >> 8;
   return result;
 }
 
 uint32_t bswap32(uint32_t x) {
   uint32_t result = 0;
-  result |= (x&0x000000FF) << 24;
-  result |= (x&0x0000FF00) << 8;
-  result |= (x&0x00FF0000) >> 8;
-  result |= (x&0xFF000000) >> 24;
+  result |= (x & 0x000000FF) << 24;
+  result |= (x & 0x0000FF00) << 8;
+  result |= (x & 0x00FF0000) >> 8;
+  result |= (x & 0xFF000000) >> 24;
   return result;
 }
 
 uint64_t bswap64(uint64_t x) {
   uint64_t result = 0;
-  result |= (x&0x00000000000000FF) << 56;
-  result |= (x&0x000000000000FF00) << 40;
-  result |= (x&0x0000000000FF0000) << 24;
-  result |= (x&0x00000000FF000000) << 8;
-  result |= (x&0x000000FF00000000) >> 8;
-  result |= (x&0x0000FF0000000000) >> 24;
-  result |= (x&0x00FF000000000000) >> 40;
-  result |= (x&0xFF00000000000000) >> 56;
+  result |= (x & 0x00000000000000FF) << 56;
+  result |= (x & 0x000000000000FF00) << 40;
+  result |= (x & 0x0000000000FF0000) << 24;
+  result |= (x & 0x00000000FF000000) << 8;
+  result |= (x & 0x000000FF00000000) >> 8;
+  result |= (x & 0x0000FF0000000000) >> 24;
+  result |= (x & 0x00FF000000000000) >> 40;
+  result |= (x & 0xFF00000000000000) >> 56;
   return result;
 }
 
--- test/data/pointers5.c
+++ test/data/pointers5.c
@@ -7,7 +7,7 @@
 int main(void) {
   int *a = (int *)malloc(sizeof(int));
   intptr_t b;
- 
+
   b = (intptr_t)a;
   *((int *)b) = 1;
   assert(*a == 1);
