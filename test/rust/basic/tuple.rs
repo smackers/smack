3a2917c02a86cb730002f4ff3add30a17c77f963
Shaobo He <polarishehn@gmail.com>
2020-05-04 23:20:48 -0600

    Added a regression for Rust tuple

---
+++ test/rust/basic/tuple.rs
@@ -0,0 +1,13 @@
+#[macro_use]
+mod smack;
+use smack::*;
+
+// @expect verified
+
+fn main() {
+  let t = (2u8.nondet(), 3u8.nondet());
+  let (a, b) = t;
+  assume!(a < 4);
+  assume!(b < 5);
+  assert!(t.0 + t.1 <= 7);
+}
