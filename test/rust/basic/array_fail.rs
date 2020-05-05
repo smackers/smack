b578f72b428938711b60bb4830adf01205e23b11
Shaobo He <polarishehn@gmail.com>
2020-05-04 23:19:51 -0600

    Added a regression for Rust array

---
+++ test/rust/basic/array_fail.rs
@@ -0,0 +1,15 @@
+#[macro_use]
+mod smack;
+use smack::*;
+
+// @expect error
+
+fn main() {
+  let ar = [2u8.nondet(), 3u8.nondet(), 4u8.nondet()];
+  let idx = 1usize.nondet();
+  assume!(ar[0] < 4);
+  assume!(ar[1] < 5);
+  assume!(ar[2] < 6);
+  assume!(idx < 3);
+  assert!(ar[idx] <= 4);
+}
