983308dc2fe59bf4c9dddf35a18e39a52e8a2af7
Zvonimir <zvonimir@cs.utah.edu>
2020-05-13 08:58:55 -0600

    We enable array theory by default
    And so we do not have to do it again for Yices2.

--- share/smack/top.py
+++ share/smack/top.py
@@ -483,8 +483,8 @@ def verify_bpl(args):
       command += ["/loopUnroll:%d" % args.unroll]
     if args.solver == 'cvc4':
       command += ["/proverOpt:SOLVER=cvc4"]
-    if args.solver == 'yices2':
-      command += ["/proverOpt:SOLVER=Yices2", "/useArrayTheory"]
+    elif args.solver == 'yices2':
+      command += ["/proverOpt:SOLVER=Yices2"]
 
   elif args.verifier == 'corral':
     command = ["corral"]
@@ -499,8 +499,8 @@ def verify_bpl(args):
     command += ["/bopt:proverOpt:O:smt.qi.eager_threshold=100"]
     if args.solver == 'cvc4':
       command += ["/bopt:proverOpt:SOLVER=cvc4"]
-    if args.solver == 'yices2':
-      command += ["/bopt:proverOpt:SOLVER=Yices2", "/bopt:useArrayTheory"]
+    elif args.solver == 'yices2':
+      command += ["/bopt:proverOpt:SOLVER=Yices2"]
 
   elif args.verifier == 'symbooglix':
     command = ['symbooglix']
