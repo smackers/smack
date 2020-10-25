356618f286d5fc47029d85976ad0f129a3b2d344
Zvonimir <zvonimir@cs.utah.edu>
2020-10-25 11:22:57 -0600

    Implemented modeling of memcmp
    I implemented it using a loop, which then has to be unrolled an
    appropriate amount of times. This could be improved if we want
    to use quantifiers, but I think this should be good for now.
    
    Closes #629

---
+++ test/c/strings/test_memcmp.c
@@ -0,0 +1,20 @@
+#include "smack.h"
+#include <string.h>
+
+// @expect verified
+
+int main(void) {
+  char word1[] = "Test memcmp";
+  char word2[] = "Test memcmp";
+  char word3[] = "Test memcpy";
+  int result;
+
+  result = memcmp(word1, word2, 11);
+  assert(result == 0);
+  result = memcmp(word1, word3, 11);
+  assert(result < 0);
+  result = memcmp(word1, word3, 8);
+  assert(result == 0);
+
+  return 0;
+}
---
+++ test/c/strings/test_memcmp_fail.c
@@ -0,0 +1,17 @@
+#include "smack.h"
+#include <string.h>
+
+// @expect error
+
+int main(void) {
+  char word1[] = "Test memcmp";
+  char word2[] = "Test memcpy";
+  char word3[] = "Test memcmp";
+  int result1, result2;
+
+  result1 = memcmp(word1, word2, 11);
+  result2 = memcmp(word1, word3, 11);
+  assert(result1 >= 0 || result2 != 0);
+
+  return 0;
+}
