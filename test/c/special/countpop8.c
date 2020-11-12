91ae19aec4b6b77552225111cddf439ce7f2016a
Mark S. Baranowski <mark.s.baranowski@gmail.com>
2020-11-11 21:56:31 -0700

    Reduce regression difficulty

--- test/c/special/countpop8.c
+++ test/c/special/countpop8.c
@@ -25,6 +25,7 @@ uint8_t ctpop8(uint8_t x) {
 
 int main(void) {
   uint8_t x = __VERIFIER_nondet_unsigned_char();
+  __VERIFIER_assume(x < 32);
   assert(__builtin_popcount(x) == ctpop8(x));
   return 0;
 }
