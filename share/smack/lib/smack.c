01e591fbd1eb21f2d1d2b9908b6e31da8cebacca
Zvonimir <zvonimir@cs.utah.edu>
2020-11-14 10:10:57 -0700

    Turned __builtin_expect into a macro that rewrites it
    The macro rewrites it into __builtinx_expect, and I provide a
    model for that procedure. Otherwise, we have name clashes with
    other headers when running on regressions.

--- share/smack/lib/smack.c
+++ share/smack/lib/smack.c
@@ -38,7 +38,7 @@ void *__builtinx_va_arg(char *x) {
   return 0;
 }
 
-long __builtin_expect(long exp, long c) { return exp; }
+long __builtinx_expect(long exp, long c) { return exp; }
 
 void __VERIFIER_assume(int x) {
 #if !RUST_EXEC
