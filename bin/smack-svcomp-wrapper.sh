2e2f4311554f39801e45895ade5e010c2eddd0f7
Zvonimir <zvonimir@cs.utah.edu>
2020-11-27 12:59:37 -0700

    Updated SMACK SVCOMP wrapper to set dotnet environment variables
    We are now bundling dotnet with SMACK. These updates make sure
    that the appropriate environment variables are set.

--- bin/smack-svcomp-wrapper.sh
+++ bin/smack-svcomp-wrapper.sh
@@ -7,8 +7,10 @@ SMACK_BIN="${ROOT}/bin"
 BOOGIE_BIN="${ROOT}/smack-deps/boogie"
 CORRAL_BIN="${ROOT}/smack-deps/corral"
 Z3_BIN="${ROOT}/smack-deps/z3/bin"
+DOTNET_BIN="${ROOT}/smack-deps/dotnet"
 
-export PATH=${SMACK_BIN}:${BOOGIE_BIN}:${CORRAL_BIN}:${Z3_BIN}:$PATH
+export DOTNET_ROOT=${DOTNET_BIN}
+export PATH=${DOTNET_BIN}:${SMACK_BIN}:${BOOGIE_BIN}:${CORRAL_BIN}:${Z3_BIN}:$PATH
 
 smack -x=svcomp --verifier=svcomp -q $@
 
