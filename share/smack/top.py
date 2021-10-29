6ba2a757a01bccb3545d2f10adcecb418f0bb855
Joshua McMillan <jem@kornat.cs.utah.edu>
2021-10-29 16:03:11 -0600

    removed whitespace

--- share/smack/top.py
+++ share/smack/top.py
@@ -17,7 +17,6 @@ from .replay import replay_error_trace
 from .frontend import link_bc_files, frontends, languages, extra_libs
 from .errtrace import error_trace, json_output_str
 
-
 VERSION = '2.7.1'
 
 
